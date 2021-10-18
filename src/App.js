1ea4bfcadb39dbc3790fb5c2f6ed3c78bf2846e7
lindseydortch <codewithlinds@gmail.com>
2021-10-18 15:39:26 -0500

    added all components for header portion

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React from 'react'
-import './App.css'
+import './App.scss'
+import { BrowserRouter, Route } from 'react-router-dom';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import Header from './components/header/Header';
 
 export default function App() {
   return (
-    <>
-     <MapContainer center={[39.2904, -76.6122]} zoom={13} scrollWheelZoom={true}>
-  <TileLayer
-    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={[39.2904, -76.6122]}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
-</MapContainer>
-    </>
+
+    <BrowserRouter >
+      <Route exact path="" component={Header} />
+      <Route exact path="/home">
+        <MapContainer center={[39.2904, -76.6122]} zoom={13} scrollWheelZoom={true}>
+        <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+        <Marker position={[39.2904, -76.6122]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+        </Marker>
+        </MapContainer>
+      </Route>
+    </BrowserRouter>
+
+    
   );
 }
 
