f8192f6ff03a1b48a3e1c08854adfc6f4c6a94e7
Bredy Evans <Bredell Evans>
2021-11-10 01:31:45 -0500

    more deployment

---
+++ src/Components/header/headerinfo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './header.scss'
+import HeaderImage from './header-image/HeaderImage'
+import LogoBox from './logo-box/LogoBox'
+import Navbar from './navbar/Navbar'
+
+const Header = () => {
+  return (
+    <div>
+      <LogoBox />
+      <Navbar />
+      <HeaderImage />
+    </div>
+  )
+}
+
+export default Header
